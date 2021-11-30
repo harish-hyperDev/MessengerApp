6b8110422088ba3a7bac3dd0084398d577a8a0bc
harish-hyperDev <coolprogrammer.777@gmail.com>
2021-11-30 16:12:34 +0530

    first commit

---
+++ build/precache-manifest.3dfa59d897dcf80536ef3023d2019444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f39167439f006d0148c011651884f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ffde4432bfc15588721",
+    "url": "/static/css/main.1b2226ec.chunk.css"
+  },
+  {
+    "revision": "0ea1dae350de0273ae13",
+    "url": "/static/js/2.533f0f78.chunk.js"
+  },
+  {
+    "revision": "5d04f8f899364246b746d38456d01275",
+    "url": "/static/js/2.533f0f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e78b7dae787ab58974",
+    "url": "/static/js/3.7502401c.chunk.js"
+  },
+  {
+    "revision": "0ffde4432bfc15588721",
+    "url": "/static/js/main.a7cf35fe.chunk.js"
+  },
+  {
+    "revision": "53bb1ccdfeb6c5114860",
+    "url": "/static/js/runtime-main.436ad786.js"
+  }
+]);
